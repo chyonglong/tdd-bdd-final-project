dbc3114439ff357ba7474b583ff93759f6840548
chyonglong <164292315@qq.com>
2025-05-28 08:28:48 +0800

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -36,6 +36,8 @@ def step_impl(context):
     #
     # List all of the products and delete them one by one
     #
+    print("Hello, World!")
+    print(context.base_url)
     rest_endpoint = f"{context.base_url}/products"
     context.resp = requests.get(rest_endpoint)
     assert(context.resp.status_code == HTTP_200_OK)
@@ -43,10 +45,13 @@ def step_impl(context):
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
-    #
-    # load the database with new products
-    #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
