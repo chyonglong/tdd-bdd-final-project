d67284e541c9fb028898b042f024724b67720723
chyonglong <164292315@qq.com>
2025-05-28 09:30:35 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -198,24 +198,6 @@ class TestProductRoutes(TestCase):
         updated_product = response.get_json()
         self.assertEqual(updated_product["description"], "unknown")
 
-######################################################################
-# DELETE A PRODUCT
-######################################################################
-@app.route("/products/<int:product_id>", methods=["DELETE"])
-def delete_products(product_id):
-    """
-    Delete a Product
-
-    This endpoint will delete a Product based the id specified in the path
-    """
-    app.logger.info("Request to Delete a product with id [%s]", product_id)
-
-    product = Product.find(product_id)
-    if product:
-        product.delete()
-
-    return "", status.HTTP_204_NO_CONTENT
-
     def test_get_product_list(self):
         """It should Get a list of Products"""
         self._create_products(5)
